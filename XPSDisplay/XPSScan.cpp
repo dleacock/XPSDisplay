82b00ba6e530868dad76badd99f4ed61f53aa3f3
dleacock <dave.leacock@gmail.com>
2015-09-14 11:52:04 -0600

    updated loadFromFile to use Qt classes instead of std

--- XPSDisplay/XPSScan.cpp
+++ XPSDisplay/XPSScan.cpp
@@ -1,5 +1,6 @@
 #include "XPSScan.h"
 #include "iostream"
+#include "QRegExp"
 
 XPSScan::XPSScan()
 {
@@ -20,8 +21,6 @@ XPSScan::XPSScan()
 */
 void XPSScan::loadFromFile(QString filePath){
 
-    qDebug() << "entering loadFromFile" << endl;
-
     QFile file(filePath);
     file.open(QIODevice::ReadOnly | QIODevice::Text);
     QTextStream inputFile(&file);
@@ -31,48 +30,45 @@ void XPSScan::loadFromFile(QString filePath){
         // Relevent data in IGOR file doesn't start until the head [Data 1] is displayed, the position of the header plus 14 spaces is where data starts.
         // Its presented as kinetic energy followed by a single space then the counts.
 
-	QString line = inputFile.readAll();
-	std::string lineString = line.toStdString();
+        QString line = inputFile.readAll();
+        // Start by finding the index of the header file. Add the length of that header file (8 spaces) and this provides the starting index of all the data needed.
+        // As of sept 12 this finds all the numbers after the data header, without any regard for spacing or decimals. My current thinking is that I find the index of
+        // the first number, find the index of the next space and store that index. Use substr (size_t pos = 0, size_t len = npos) to clip out the number, convert it
+        // to qreal then store in vector. Repeat for next digit then on to the next line.
 
 
-	// Start by finding the index of the header file. Add the length of that header file (8 spaces) and this provides the starting index of all the data needed.
-	// As of sept 12 this finds all the numbers after the data header, without any regard for spacing or decimals. My current thinking is that I find the index of
-	// the first number, find the index of the next space and store that index. Use substr (size_t pos = 0, size_t len = npos) to clip out the number, convert it
-	// to qreal then store in vector. Repeat for next digit then on to the next line.
-	std::size_t indexOfDataHeader = lineString.find("[Data 1]");
-	if( indexOfDataHeader != std::string::npos){
+        int indexOfDataHeader = line.indexOf("[Data 1]");
+        qDebug() << "indexOfDataHeader: " << indexOfDataHeader;
+        if( indexOfDataHeader != -1){
 
-		std::size_t indexFound = lineString.find_first_of("0123456789", indexOfDataHeader + 8);
-		while(indexFound != std::string::npos){
+            QRegExp regNum("([0-9])");
 
-			// Currently at beginning of important data. Grab current index. Find next space, grab that index
-			std::size_t indexStartKinetic = indexFound;
-			std::size_t indexEndKinetic = lineString.find_first_of(" ", indexStartKinetic)-1;
+            int indexFound = line.indexOf(regNum, indexOfDataHeader + 8);
 
-			std::string kineticString = lineString.substr(indexStartKinetic, indexEndKinetic-indexStartKinetic+1);
+            while(indexFound != -1){
 
+                // Currently at beginning of important data. Grab current index. Find next space, grab that index
+                int indexStartKinetic = indexFound;
+                int indexEndKinetic = line.indexOf(" ", indexStartKinetic) - 1;
+                QString kineticString = line.mid(indexStartKinetic, indexEndKinetic-indexStartKinetic + 1);
 
-			// Jump to start of next group of numbers. Get index, find and store the last index
-			std::size_t indexStartCounts = lineString.find_first_of("0123456789", indexEndKinetic+1);
-			std::size_t indexEndCounts = lineString.find_first_of('\n', indexStartCounts)-1;
+                // Jump to start of next group of numbers. Get index, find and store the last index
+                int indexStartCounts = line.indexOf(regNum, indexEndKinetic + 1);
+                int indexEndCounts = line.indexOf('\n', indexStartCounts) - 1;
+                QString countsString = line.mid(indexStartCounts, indexEndCounts-indexStartCounts + 1);
 
-			std::string countsString = lineString.substr(indexStartCounts, indexEndCounts-indexStartCounts+1);
+                //DEBUG: output values
+                qDebug() << kineticString << " " << countsString;
 
-			//DEBUG: output values
-			std::cout << kineticString << " " << countsString << std::endl;
-
-			//ToDo: Convert kineticString and countsString to qreal then store in vectors
+                //ToDo: Convert kineticString and countsString to qreal then store in vectors
 
 
-			// Update index then restart loop
-			indexFound = lineString.find_first_of("0123456789", indexEndCounts+1);
+                // Update index then restart loop
+                indexFound = line.indexOf(regNum, indexEndCounts+1);
 
-		}
-	}
+            }
+        }
 
-
-        // I might need to convert the QString to a std::string so I can use the standard librarys find functions to help parse
-	//std::string copyTextString = copyText.toUtf8().constData();
     }
 
     file.close();
